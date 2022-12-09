64eac9693be84b6f08bb0b80064e17f6f2a83d59
mh17yl <mh17yl@brocku.ca>
2022-12-09 00:14:49 -0500

    n

---
+++ Assignments/final1.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("para").style.color = "pink";
+
+document.getElementById("para").style.fontSize = "1px";
+
+
+let now = new Date();
+alert( now ); // shows current date/time
